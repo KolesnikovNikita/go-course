2c33d65fe6da407897a9a641c226ea24b0ccdfdd
Nikita Kolesnikov <nik.kolesnikov.andrew@gmail.com>
2023-07-10 20:08:30 +0300

    last commit

--- cmd/gosearch/gosearch.go
+++ cmd/gosearch/gosearch.go
@@ -37,7 +37,7 @@ func main() {
 	flag.Parse()
 
 	if *searchWord == "" {
-		fmt.Println("Не указано слово для поиска, укажите пожалуйста")
+		fmt.Println("Не указано слово для поиска")
 		return
 	} else {
 		match := checkUrlsForWord(urlList, *searchWord)
